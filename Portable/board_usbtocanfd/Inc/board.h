52297f41576361fff0204b1526da88507bc9a2e2
Phil Greenland <phil@beamconnectivity.com>
2023-09-26 18:04:13 +0100

    Add board definition

---
+++ Portable/board_usbtocanfd/Inc/board.h
@@ -0,0 +1,124 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2022 Ryan Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "usbd_def.h"
+
+#define BOARD_SYSMEM_RESET_VECTOR 0x1FFF0000
+
+#define USBD_VID				  0x1D50
+#define USBD_LANGID_STRING		  1033
+#define USBD_MANUFACTURER_STRING  (uint8_t*) "Quantulum Ltd"
+#define USBD_PID_FS				  0x606F
+#define USBD_PRODUCT_STRING_FS	  (uint8_t*) "USBtoCANFD"
+#define USBD_CONFIGURATION_STRING (uint8_t*) "gs_usb"
+#define USBD_INTERFACE_STRING	  (uint8_t*) "gs_usb interface"
+#define DFU_INTERFACE_STRING	  (uint8_t*) "USBtoCANFD DFU interface"
+
+/* FDCAN init values for this board */
+#define FDCAN_SJW_INIT				 1
+#define FDCAN_BRP_INIT				 8
+#define FDCAN_TS1_INIT				 13
+#define FDCAN_TS2_INIT				 2
+
+#define FDCAN_DATA_SJW_INIT			 4
+#define FDCAN_DATA_BRP_INIT			 2
+#define FDCAN_DATA_TS1_INIT			 15
+#define FDCAN_DATA_TS2_INIT			 4
+
+#define FDCAN_CLOCK_DIV_INIT		 FDCAN_CLOCK_DIV1
+#define FDCAN_FRAME_FMT_INIT		 FDCAN_FRAME_FD_BRS
+#define FDCAN_MODE_INIT				 FDCAN_MODE_NORMAL
+#define FDCAN_AUTO_RETX_INIT		 DISABLE
+#define FDCAN_AUTO_TX_PAUSE_INIT	 DISABLE
+#define FDCAN_PROT_EXCPTN_INIT		 ENABLE
+#define FDCAN_STD_FLTR_NUM_INIT		 0
+#define FDCAN_EXT_FLTR_NUM_INIT		 0
+#define FDCAN_TX_FIFO_OPERATION_INIT FDCAN_TX_FIFO_OPERATION
+
+#define CAN_NUM_CHANNELS			 2
+#define CAN_CLOCK_SPEED				 80000000
+#define CAN_TERM_FEATURE_ENABLED
+#define CANFD_FEATURE_ENABLED
+
+#define QUEUE_SIZE_HOST_TO_DEV	  32
+#define QUEUE_SIZE_DEV_TO_HOST	  32
+
+#define CAN1_TX_Pin GPIO_PIN_9
+#define CAN1_TX_GPIO_Port GPIOB
+#define OSC_IN_Pin GPIO_PIN_14
+#define OSC_IN_GPIO_Port GPIOC
+#define CAN1_STBY_Pin GPIO_PIN_0
+#define CAN1_STBY_GPIO_Port GPIOA
+#define CAN2_STBY_Pin GPIO_PIN_1
+#define CAN2_STBY_GPIO_Port GPIOA
+#define UART_TX_Pin GPIO_PIN_2
+#define UART_TX_GPIO_Port GPIOA
+#define UART_RX_Pin GPIO_PIN_3
+#define UART_RX_GPIO_Port GPIOA
+#define CAN2_RX_Pin GPIO_PIN_0
+#define CAN2_RX_GPIO_Port GPIOB
+#define CAN2_TX_Pin GPIO_PIN_1
+#define CAN2_TX_GPIO_Port GPIOB
+#define LED_PWR_Pin GPIO_PIN_9
+#define LED_PWR_GPIO_Port GPIOA
+#define CAN_PWR_EN_Pin GPIO_PIN_6
+#define CAN_PWR_EN_GPIO_Port GPIOC
+#define USB_DM_Pin GPIO_PIN_11
+#define USB_DM_GPIO_Port GPIOA
+#define USB_DP_Pin GPIO_PIN_12
+#define USB_DP_GPIO_Port GPIOA
+#define SWDIO_Pin GPIO_PIN_13
+#define SWDIO_GPIO_Port GPIOA
+#define SWCLK_BOOT0_Pin GPIO_PIN_14
+#define SWCLK_BOOT0_GPIO_Port GPIOA
+#define LED_CAN1_RX_Pin GPIO_PIN_3
+#define LED_CAN1_RX_GPIO_Port GPIOB
+#define LED_CAN1_TX_Pin GPIO_PIN_4
+#define LED_CAN1_TX_GPIO_Port GPIOB
+#define LED_CAN2_TX_Pin GPIO_PIN_5
+#define LED_CAN2_TX_GPIO_Port GPIOB
+#define LED_CAN2_RX_Pin GPIO_PIN_6
+#define LED_CAN2_RX_GPIO_Port GPIOB
+#define CAN1_RX_Pin GPIO_PIN_8
+#define CAN1_RX_GPIO_Port GPIOB
+
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ BOARD_H__ */
+
---
+++ Portable/board_usbtocanfd/Src/board.c
@@ -0,0 +1,250 @@
+/*
+
+The MIT License (MIT)
+
+Copyright (c) 2022 Ryan Edwards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "board.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "usbd_gs_can.h"
+#include "can.h"
+#include "led.h"
+
+static LED_HandleTypeDef hled_pwr;
+static LED_HandleTypeDef hled_can1rx;
+static LED_HandleTypeDef hled_can1tx;
+static LED_HandleTypeDef hled_can2rx;
+static LED_HandleTypeDef hled_can2tx;
+
+FDCAN_HandleTypeDef hfdcan1;
+FDCAN_HandleTypeDef hfdcan2;
+
+extern USBD_GS_CAN_HandleTypeDef hGS_CAN;
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE|RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV1;
+  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV4;
+  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV5;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/** Configure pins
+*/
+void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, CAN1_STBY_Pin|CAN2_STBY_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LED_PWR_GPIO_Port, LED_PWR_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(CAN_PWR_EN_GPIO_Port, CAN_PWR_EN_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, LED_CAN1_RX_Pin|LED_CAN1_TX_Pin|LED_CAN2_TX_Pin|LED_CAN2_RX_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : CAN1_STBY_Pin CAN2_STBY_Pin LED_PWR_Pin */
+  GPIO_InitStruct.Pin = CAN1_STBY_Pin|CAN2_STBY_Pin|LED_PWR_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : CAN_PWR_EN_Pin */
+  GPIO_InitStruct.Pin = CAN_PWR_EN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(CAN_PWR_EN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : LED_CAN1_RX_Pin LED_CAN1_TX_Pin LED_CAN2_TX_Pin LED_CAN2_RX_Pin */
+  GPIO_InitStruct.Pin = LED_CAN1_RX_Pin|LED_CAN1_TX_Pin|LED_CAN2_TX_Pin|LED_CAN2_RX_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+void main_init_cb(void)
+{
+	/* Enable power led */
+	HAL_GPIO_WritePin(LED_PWR_GPIO_Port, LED_PWR_Pin, GPIO_PIN_SET);
+
+	/* Startup RX / TX led dance */
+	for (uint8_t i=0; i<20; i++)
+	{
+		HAL_GPIO_TogglePin(LED_CAN1_RX_GPIO_Port, LED_CAN1_RX_Pin);
+		HAL_GPIO_TogglePin(LED_CAN2_RX_GPIO_Port, LED_CAN2_RX_Pin);
+		HAL_Delay(50);
+		HAL_GPIO_TogglePin(LED_CAN1_TX_GPIO_Port, LED_CAN1_TX_Pin);
+		HAL_GPIO_TogglePin(LED_CAN2_TX_GPIO_Port, LED_CAN2_TX_Pin);
+	}
+
+	/* Init CAN channels */
+	hGS_CAN.channels[0] = &hfdcan1;
+	hGS_CAN.channels[1] = &hfdcan2;
+	can_init(hGS_CAN.channels[0], FDCAN1);
+	can_init(hGS_CAN.channels[1], FDCAN2);
+
+	/* Init LEDs */
+	led_init(&hled_pwr, LED_PWR_GPIO_Port, LED_PWR_Pin, LED_MODE_ACTIVE, LED_ACTIVE_HIGH);
+	led_init(&hled_can1rx, LED_CAN1_RX_GPIO_Port, LED_CAN1_RX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
+	led_init(&hled_can1tx, LED_CAN1_TX_GPIO_Port, LED_CAN1_TX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
+	led_init(&hled_can2rx, LED_CAN2_RX_GPIO_Port, LED_CAN2_RX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
+	led_init(&hled_can2tx, LED_CAN2_TX_GPIO_Port, LED_CAN2_TX_Pin, LED_MODE_INACTIVE, LED_ACTIVE_HIGH);
+}
+
+void main_task_cb(void)
+{
+	/* update all the LEDs */
+	led_update(&hled_pwr);
+	led_update(&hled_can1rx);
+	led_update(&hled_can1tx);
+	led_update(&hled_can2rx);
+	led_update(&hled_can2tx);
+}
+
+void can_on_enable_cb(uint8_t channel)
+{
+	if (channel == 0) {
+		HAL_GPIO_WritePin(CAN1_STBY_GPIO_Port, CAN1_STBY_Pin, GPIO_PIN_RESET);
+	}
+	if (channel == 1) {
+		HAL_GPIO_WritePin(CAN2_STBY_GPIO_Port, CAN2_STBY_Pin, GPIO_PIN_RESET);
+	}
+}
+
+void can_on_disable_cb(uint8_t channel)
+{
+	if (channel == 0) {
+		HAL_GPIO_WritePin(CAN1_STBY_GPIO_Port, CAN1_STBY_Pin, GPIO_PIN_SET);
+	}
+	if (channel == 1) {
+		HAL_GPIO_WritePin(CAN2_STBY_GPIO_Port, CAN2_STBY_Pin, GPIO_PIN_SET);
+	}
+}
+
+void can_on_tx_cb(uint8_t channel, struct gs_host_frame *frame)
+{
+	UNUSED(frame);
+	if (channel == 0)
+		led_indicate_rxtx(&hled_can1tx);
+	else
+		led_indicate_rxtx(&hled_can2tx);
+}
+
+void can_on_rx_cb(uint8_t channel, struct gs_host_frame *frame)
+{
+	UNUSED(frame);
+	if (channel == 0)
+		led_indicate_rxtx(&hled_can1rx);
+	else
+		led_indicate_rxtx(&hled_can2rx);
+}
+
+void can_identify_cb(uint32_t do_identify)
+{
+	if (do_identify) {
+		led_blink_start(&hled_pwr, 250);
+	}
+	else {
+		led_blink_stop(&hled_pwr);
+	}
+}
+
+static uint8_t can_term_pwr_flags;
+#define CAN_TERM_PWR_FLAGS_BOTH_INTFC (0x03)
+
+void can_set_term_cb(uint8_t channel, GPIO_PinState state)
+{
+	if (GPIO_PIN_SET == state) {
+		can_term_pwr_flags |= (1U << channel);
+	} else {
+		can_term_pwr_flags &= ~(1U << channel);
+	}
+
+	/* enable can power if termination is specified for both busses (bit of a hack...ssh)*/
+	if (CAN_TERM_PWR_FLAGS_BOTH_INTFC == can_term_pwr_flags) {
+		HAL_GPIO_WritePin(CAN_PWR_EN_GPIO_Port, CAN_PWR_EN_Pin, GPIO_PIN_SET);
+	} else {
+		HAL_GPIO_WritePin(CAN_PWR_EN_GPIO_Port, CAN_PWR_EN_Pin, GPIO_PIN_RESET);
+	}
+}
+
+GPIO_PinState can_get_term_cb(uint8_t channel)
+{
+	return (CAN_TERM_PWR_FLAGS_BOTH_INTFC == can_term_pwr_flags ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
